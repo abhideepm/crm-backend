1dd6932446c28b4a38b399683055df2068326c83
Abhideep Maity <abhideepm@gmail.com>
2020-07-25 19:59:20 +0530

    added reset password

--- routes/password.js
+++ routes/password.js
@@ -4,6 +4,7 @@ const nodemailer = require('nodemailer')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const connectDB = require('../config/db')
+const bcrypt = require('bcrypt')
 const { ObjectId } = require('mongodb')
 
 require('dotenv').config()
@@ -16,11 +17,21 @@ const url = 'abc'
 
 const forgotPasswordEmail = (firstname, url) =>
 	`<div>
-				<h3>Dear ${firstname},</h3>
-				<p>You requested for a password reset.</p>
-				<p>Kindly use this <a href=${url}>link</a> to reset your password</p>
-				<br>
-				<p>Thank You</p>
+		<h3>Dear ${firstname},</h3>
+		<p>You requested for a password reset.</p>
+		<p>Kindly use this <a href=${url}>link</a> to reset your password</p>
+		<br>
+		<p>Thank You</p>
+	</div>`
+
+const resetPasswordConfirmation = firstname =>
+	`<div>
+		<h3>Dear ${firstname},</h3>
+		<p>Your password has been successful reset, you can now login with your new password.</p>
+		<br>
+		<div>
+			Cheers!
+		</div>
 	</div>`
 
 const sendMail = async mailOptions => {
@@ -67,7 +78,7 @@ router.post('/forgotpassword', async (req, res) => {
 
 		const mailOptions = {
 			from: `CRM Support<crmsupport@crm.com>`,
-			to: `abhideepm@gmail.com`,
+			to: user.email,
 			subject: `Reset Password request`,
 			html: forgotPasswordEmail(user.firstname, url),
 		}
@@ -80,4 +91,42 @@ router.post('/forgotpassword', async (req, res) => {
 	}
 })
 
+router.post('/resetpassword', async (req, res) => {
+	try {
+		const { token, password } = req.body
+		const db = await connectDB()
+		const collection = db.collection('users')
+		const user = await collection.findOne({
+			reset_password_token: token,
+			reset_password_expires: { $gt: Date.now() },
+		})
+		if (!user) return res.status(408).json({ message: 'Token expired' })
+
+		hash = await bcrypt.hash(password, 10)
+		user.reset_password_token = undefined
+		user.reset_password_expires = undefined
+		await collection.findOneAndUpdate(
+			{ _id: user._id },
+			{
+				$set: {
+					password: hash,
+				},
+				$unset: {
+					reset_password_expires: '',
+					reset_password_token: '',
+				},
+			}
+		)
+		const mailOptions = {
+			from: `CRM Support<crmsupport@crm.com>`,
+			to: user.email,
+			subject: `Reset Password Successful`,
+			html: resetPasswordConfirmation(user.firstname),
+		}
+		sendMail(mailOptions)
+		res.json({ message: 'Password successfully updated' })
+	} catch (err) {
+		res.json({ message: error })
+	}
+})
 module.exports = router
