97a1d21779069c5b29b433a5218f29b564e5dc8b
Abhideep Maity <abhideepm@gmail.com>
2020-07-26 21:01:44 +0530

    auth endpoint bug fix

--- app.js
+++ app.js
@@ -26,6 +26,23 @@ app.use('/leads', leadRouter)
 app.use('/contacts', contactRouter)
 app.use('/requests', contactRouter)
 
-app.route('/auth',auth)
+app.get('/auth',(req,res)=>{
+  const token = req.headers.authenticate
+	try {
+		if (!token) {
+			return res.status(401).json({ message: 'Unauthorized access' })
+		}
+
+		const user = await jwt.verify(token, process.env.secret)
+    const info = {}
+    info[id] = user.id
+		info[email] = user.email
+		info[role] = user.role
+		res.json(info)
+	} catch (err) {
+		console.log('Error with middleware')
+		res.status(500).json({ message: 'Token Expired' })
+	}
+})
 
 app.listen(PORT, () => console.log('Listening'))
